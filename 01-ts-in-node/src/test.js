d984d924e150d83726828b58f057443483030c1d
Renke Brixel <renke.brixel@gmail.com>
2025-12-19 14:33:52 +0100

    add node in ts lecture

---
+++ 01-ts-in-node/src/test.js
@@ -0,0 +1,11 @@
+// const express = require("express");
+// require("dotenv").config();
+
+// import express from "express";
+
+console.log("Hello from Node!");
+console.log("Hello again");
+
+function add(a, b) {
+  return a + b;
+}
